29daceb09a9199dec814044b77c06513affb00af
KaranPannu668 <karanpannu668@gmail.com>
2022-07-01 18:25:32 +0530

    First Commit

---
+++ src/pages/Landing.js
@@ -0,0 +1,25 @@
+import main from '../assets/images/main.svg'
+import Wrapper from '../assets/wrappers/LandingPage'
+import { Logo } from '../components'
+import {Link} from 'react-router-dom'
+
+const Landing = () => {
+    return <Wrapper>
+        <nav>
+            <Logo />
+        </nav>
+        <div className="container page">
+        {/* Info */}
+        <div className="info">
+            <h1>job <span>tracking</span> app</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium earum aperiam, quasi sequi temporibus magnam minus facere adipisci maiores dolore.</p>
+            <Link to='/register' className='btn btn-hero' >
+                Login/Register
+            </Link>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+        </div>
+    </Wrapper>
+}
+
+export default Landing
